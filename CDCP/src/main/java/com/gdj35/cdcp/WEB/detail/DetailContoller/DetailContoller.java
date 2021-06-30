10e7a9d40b848fa5d72ea25a44f181618b185200
장원혁 <seiou@naver.com>
2021-06-30 14:01:33 +0900

    떙겨봐

--- CDCP/src/main/java/com/gdj35/cdcp/WEB/detail/DetailContoller/DetailContoller.java
+++ CDCP/src/main/java/com/gdj35/cdcp/WEB/detail/DetailContoller/DetailContoller.java
@@ -1,5 +1,24 @@
 package com.gdj35.cdcp.WEB.detail.DetailContoller;
 
-public class DetailContoller {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+
+public class DetailContoller {
+	
+	@RequestMapping(value="/detail")
+	
+	public ModelAndView detail(ModelAndView mav) {
+		
+		
+		mav.setViewName("detail/detail");
+		
+		return mav;
+		
+		
+	}
+	
+	
 }
