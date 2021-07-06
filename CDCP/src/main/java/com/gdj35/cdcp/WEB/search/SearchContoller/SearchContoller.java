c5286289a74088a1963f500cdb53900f0cbb4dc5
유시연 <syou0296@naver.com>
2021-07-06 15:02:52 +0900

    id/pw찾기 메일인증기능

--- CDCP/src/main/java/com/gdj35/cdcp/WEB/search/SearchContoller/SearchContoller.java
+++ CDCP/src/main/java/com/gdj35/cdcp/WEB/search/SearchContoller/SearchContoller.java
@@ -1,30 +1,11 @@
 package com.gdj35.cdcp.WEB.search.SearchContoller;
 
-import java.util.HashMap;
-import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gdj35.cdcp.WEB.user.UserService.UserIService;
-import com.gdj35.cdcp.util.Mail;
 
 @Controller
 public class SearchContoller {
-	@Autowired UserIService useriService;
 	
 	//맞춤카드검색 페이지
 	@RequestMapping(value = "/checkSearch")
@@ -51,34 +32,6 @@ public class SearchContoller {
 	@RequestMapping(value = "/searchingCardList")
 	public ModelAndView searchingCardList(ModelAndView mav) {
 		mav.setViewName("search/searchingCardList");
-		return mav;
-	}
-	
-	//user - searchmem 이메일인증
-	@RequestMapping(value = "/checkingEmail", method = RequestMethod.POST, produces = "text/json;charset=UTF-8")
-	@ResponseBody
-	public String checkingEmail(@RequestParam HashMap<String,String> params) throws Throwable {
-		ObjectMapper mapper = new ObjectMapper();
-		HashMap<String, Object> modelMap = new HashMap<String, Object>();
-		
-		try {
-			HashMap<String, String> data = useriService.getId(params); 
-			
-			if (params.get("findType") == "id") {
-				Mail.sendMail(params.get("checkEmail"), params.get("findType"), data.get("MEMBER_ID"));
-			} else if (params.get("findType") == "pw") {
-				
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return mapper.writeValueAsString(modelMap);
-	}
-	
-	//blur샘플
-	@RequestMapping(value = "/blurSample")
-	public ModelAndView blurSample(ModelAndView mav) {
-		mav.setViewName("user/blurSample");
 		return mav;
 	}
 }
--- CDCP/src/main/java/com/gdj35/cdcp/util/Mail.java
+++ CDCP/src/main/java/com/gdj35/cdcp/util/Mail.java
@@ -10,12 +10,14 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import org.apache.commons.lang.RandomStringUtils;
+
 public class Mail {
-	public static void sendMail(String email, String type, String id) {
+	public static void sendMail(String email, String type, String sendTxt) {
 		String host = "smtp.naver.com";
 		int port=465; //1. 네이버 STMP 포트 번호
-		String user = "lauren1231";	//2. 발신자 네이버 아이디(@이후는 쓰지 말 것)
-		String password = "apples1289";		//3. 발신자 네이버 비밀번호
+		String user = "";	//2. 발신자 네이버 아이디(@이후는 쓰지 말 것)
+		String password = "";		//3. 발신자 네이버 비밀번호
 		Properties props = System.getProperties();
 		
         props.put("mail.smtp.host", host);
@@ -39,12 +41,12 @@ public class Mail {
 	        String mailTitle = "";
 	        String mailContent = "";
 	        
-	        if(email == "id") {
-	        	mailTitle = "카드캡틴 ID 찾기";
-	        	mailContent = "가입하실 때 입력하신 아이디는 " + id + "입니다.";
-	        } else if (email == "pw") {
-	        	mailTitle = "비밀번호 찾기 인증번호를 입력해 주세요";
-	        	mailContent = "";
+	        if(type.equals("id")) {
+	        	mailTitle = "[ 카드캡틴 아이디 찾기 ]";
+	        	mailContent = "가입하실 때 입력하신 아이디는 " + sendTxt + "입니다.";
+	        } else if (type.equals("pw")) {
+	        	mailTitle = "[ 카드캡틴 비밀번호 찾기 ]";
+	        	mailContent = "임시번호 입력칸에 " + sendTxt + "를 입력해 주세요.";
 	        }
 	        
 	        mimeMessage.setSubject(mailTitle);	//5. 메일 제목
