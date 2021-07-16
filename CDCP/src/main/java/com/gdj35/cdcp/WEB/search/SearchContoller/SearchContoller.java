1f68f4f7704977224b1915cea910d88be3214fa5
유시연 <syou0296@naver.com>
2021-07-16 18:51:12 +0900

    Search 	-Controller 	-XML 	페이징 수정

--- CDCP/src/main/java/com/gdj35/cdcp/WEB/search/SearchContoller/SearchContoller.java
+++ CDCP/src/main/java/com/gdj35/cdcp/WEB/search/SearchContoller/SearchContoller.java
@@ -49,30 +49,33 @@ public class SearchContoller {
 		int page = 1;
 		
 		if(params.get("searchType").equals("keyword")) {//직접검색
+			//========== 페이징 Start
 			if(params.get("page") != null) {				
 				page = Integer.parseInt(params.get("page"));
 			}
-			List<HashMap<String, String>> searchKeyword = iservice.searchKeyword(params);//중복제거 없이 모든 카드정보를 담아옴
-			List<HashMap<String, String>> searchKeywordAllCardNo = iservice.cardNoDistinct(params);//중복제거하여 모든 카드정보를 담아옴
+			List<HashMap<String, String>> searchKeyword = iservice.searchKeyword(params);//중복제거 없이 모든 카드정보를 담아옴(li 반복을 위하여)
+			List<HashMap<String, String>> cardNoDistinct = iservice.cardNoDistinct(params);
 			
-			int cnt = searchKeywordAllCardNo.size();
+			int cnt = cardNoDistinct.size();
 			
 			PagingBean pb = ipage.getPagingBean(page, cnt);
 		
 			params.put("startCnt", Integer.toString(pb.getStartCount()));
 			params.put("endCnt", Integer.toString(pb.getEndCount()));
 			
-			List<HashMap<String, String>> pagingDistinct = iservice.pagingDistinct(params);//중복제거하여 모든 카드정보를 담아옴
+			List<HashMap<String, String>> pagingDistinct = iservice.pagingDistinct(params);//화면에 보여줄 10개의 중복제거된 데이터
 
-			mav.addObject("option", params.get("option"));
-			mav.addObject("searchType", params.get("searchType"));
-			mav.addObject("options", option + " 관련 카드");
 			mav.addObject("searchKeyword",searchKeyword);//중복제거 없이 모든 카드정보를 담아옴(li 반복을 위하여)
 			mav.addObject("pagingDistinct",pagingDistinct);//화면에 보여줄 10개의 중복제거된 데이터
-			mav.addObject("searchCardNo",searchKeywordAllCardNo);//중복제거하여 모든 카드정보를 담아옴(보여줄 게시물 수)
+			
 			mav.addObject("pb", pb);
 			mav.addObject("cnt",cnt);
 			mav.addObject("page",page);
+			//========== 페이징 End
+			
+			mav.addObject("option", params.get("option"));
+			mav.addObject("searchType", params.get("searchType"));
+			mav.addObject("options", option + " 관련 카드");
 		} else if (params.get("searchType").equals("optionClick")) {//옵션선택
 			mav.addObject("options", option);
 		}
